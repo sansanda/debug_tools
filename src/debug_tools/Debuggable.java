7166374a7571dd2053ceef754da7dba6fb3ba530
DavidS <dsanchezsanc@uoc.edu>
2021-01-16 11:06:15 +0100

    first commit

---
+++ src/debug_tools/Debuggable.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package debug_tools;
+
+/**
+ * @author DavidS
+ *
+ */
+public interface Debuggable {
+	public int getDebug_level();
+	public void setDebug_level(int debug_level);
+}
